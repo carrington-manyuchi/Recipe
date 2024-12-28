5fbcbacb6965376c1e43d90891c42ebcdfdfd5df
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-28 08:26:54 +0200

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Manyuchi, Carrington C on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
